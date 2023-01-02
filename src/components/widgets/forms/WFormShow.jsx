3eeb25218495d8eb5ed3319185a76c92c678d1d0
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2023-01-03 00:29:57 +0300

    bug fixes

--- src/components/widgets/forms/WFormShow.jsx
+++ src/components/widgets/forms/WFormShow.jsx
@@ -1,34 +1,37 @@
-import { Show, TextField, SimpleShowLayout, ArrayField, Datagrid, useTranslate, BooleanField } from "react-admin";
+import { Show, TextField, SimpleShowLayout, ArrayField, Datagrid, BooleanField, SelectField } from "react-admin";
 
-const Answers = () => {
-    const translate = useTranslate();
-    return (
-        <ArrayField source="answers">
-            <Datagrid bulkActionButtons={false}>
-                <TextField source="text" label={translate("resources.surveys.fields.question.answer.text")} />
-                <TextField source="code" label={translate("resources.surveys.fields.question.answer.code")} />
-            </Datagrid>
-        </ArrayField>
-    );
-};
-const WFormShow = () => {
-    const translate = useTranslate();
-    return (
-        <Show>
-            <SimpleShowLayout>
-                <TextField source="id" fullWidth />
-                <TextField source="title" fullWidth />
-                <TextField source="description" fullWidth />
-                <ArrayField source="questions">
-                    <Datagrid bulkActionButtons={false} expand={<Answers />}>
-                        <TextField source="title" label={translate("resources.forms.fields.question.title")} />
-                        <TextField source="type" label={translate("resources.forms.fields.question.type")} />
-                        <BooleanField source="required" fullWidth />
-                    </Datagrid>
-                </ArrayField>
-            </SimpleShowLayout>
-        </Show>
-    );
-};
+const Answers = () => (
+    <ArrayField source="answers">
+        <Datagrid bulkActionButtons={false}>
+            <TextField source="text" label="resources.surveys.fields.question.answer.text" />
+            <TextField source="code" label="resources.surveys.fields.question.answer.code" />
+        </Datagrid>
+    </ArrayField>
+);
+
+const WFormShow = () => (
+    <Show>
+        <SimpleShowLayout>
+            <TextField source="id" fullWidth />
+            <TextField source="title" fullWidth />
+            <TextField source="description" fullWidth />
+            <ArrayField source="questions">
+                <Datagrid bulkActionButtons={false} expand={<Answers />}>
+                    <TextField source="title" label="resources.forms.fields.question.title" />
+                    <SelectField
+                        source="type"
+                        label="resources.forms.fields.question.type"
+                        choices={[
+                            { id: "single", name: "resources.forms.questionTypes.single" },
+                            { id: "multiple", name: "resources.forms.questionTypes.multiple" },
+                            { id: "open", name: "resources.forms.questionTypes.open" }
+                        ]}
+                    />
+                    <BooleanField source="required" fullWidth />
+                </Datagrid>
+            </ArrayField>
+        </SimpleShowLayout>
+    </Show>
+);
 
 export default WFormShow;
--- src/components/widgets/forms/WSurveyEditable.jsx
+++ src/components/widgets/forms/WSurveyEditable.jsx
@@ -1,13 +1,12 @@
-import { Form, TextInput, SaveButton } from "react-admin";
-import { Grid } from '@mui/material';
-import UIDateTimeInput from "../../ui/inputs/UIDateTimeInput";
+import { Form } from "react-admin";
+import { Grid } from "@mui/material";
 import UIMapInput from "../../ui/inputs/UIMapInput";
 
 const WSurveyEditable = () => (
     <Form>
         <Grid container columnSpacing={2} sx={{ p: 2 }}>
             <Grid item xs={12}>
-                <UIMapInput width="100%" height="400px"/>
+                <UIMapInput width="100%" height="400px" />
             </Grid>
         </Grid>
     </Form>
