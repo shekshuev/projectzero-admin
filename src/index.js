12679111fdfa52c7fd36a20a15798cfcd4e8751c
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2023-01-10 21:49:18 +0300

    test message

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ let DefaultIcon = L.icon({
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
+console.log(process.env);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
