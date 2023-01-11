6238bc546dd379de25b8a86c27b9845cdac41017
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2023-01-11 16:49:05 +0300

    ui datetime input fix

--- src/components/ui/inputs/UIDateTimeInput.jsx
+++ src/components/ui/inputs/UIDateTimeInput.jsx
@@ -1,13 +1,13 @@
-import TextField from '@mui/material/TextField';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import TextField from "@mui/material/TextField";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { useInput, useTranslate, useResourceContext, useLocaleState } from "react-admin";
-import ru from 'date-fns/locale/ru';
-import en from 'date-fns/locale/en-US';
+import ru from "date-fns/locale/ru";
+import en from "date-fns/locale/en-US";
 
 const translations = { ru, en };
 
-const UIDateTimeInput = (props) => {
+const UIDateTimeInput = props => {
     const { onChange, onBlur, ...rest } = props;
     const {
         field,
@@ -17,31 +17,33 @@ const UIDateTimeInput = (props) => {
     } = useInput({
         onChange,
         onBlur,
-        ...props,
+        ...props
     });
     const [locale] = useLocaleState();
-    const resource = useResourceContext()
+    const resource = useResourceContext();
     const translate = useTranslate();
     const i18nRoute = `resources.${resource}.fields.${field.name}`;
     let label = translate(i18nRoute);
     label = label === i18nRoute ? field.name : label;
     label = props.label ? props.label : label;
     return (
-        <LocalizationProvider dateAdapter={ AdapterDateFns }
-                              adapterLocale={ translations[locale] }>
-            <DateTimePicker { ...field }
-                            label={ label }
-                            value={ field.value ?? null }
-                            renderInput={(props) =>
-                                <TextField error={(isTouched || isSubmitted) && invalid}
-                                           helperText={(isTouched || isSubmitted) && invalid ? error : ''}
-                                           required={isRequired}
-                                           { ...rest }
-                                           { ...props }
-                            />}
+        <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={translations[locale]}>
+            <DateTimePicker
+                {...field}
+                label={label}
+                value={field.value ?? null}
+                renderInput={props => (
+                    <TextField
+                        helperText={(isTouched || isSubmitted) && invalid ? error : ""}
+                        required={isRequired}
+                        {...rest}
+                        {...props}
+                        error={(isTouched || isSubmitted) && invalid}
+                    />
+                )}
             />
         </LocalizationProvider>
-    )
-}
+    );
+};
 
 export default UIDateTimeInput;
